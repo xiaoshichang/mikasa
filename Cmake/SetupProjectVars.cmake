5e4ae303ef4ed8d5410e938161d7ff9667bae1ac
xiaoshichang <511680777@qq.com>
2024-07-10 16:32:23 +0800

    add imgui library

--- Cmake/SetupProjectVars.cmake
+++ Cmake/SetupProjectVars.cmake
@@ -4,12 +4,6 @@ message(STATUS "=============== Setup Project Vars ===================")
 set(ThirdPartySourceRoot ${CMAKE_SOURCE_DIR}/ThirdPartySource)
 message(STATUS "ThirdPartySourceRoot: ${ThirdPartySourceRoot}")
 
-set(ThirdPartyLibraryRoot ${CMAKE_SOURCE_DIR}/ThirdPartyLibrary)
-message(STATUS "ThirdPartyLibraryRoot: ${ThirdPartyLibraryRoot}")
-
-set(ThirdPartySupportRoot ${CMAKE_SOURCE_DIR}/ThirdPartySupport)
-message(STATUS "ThirdPartySupportRoot: ${ThirdPartySupportRoot}")
-
 set(EngineDllName MikasaEngine.${MIKASA_TARGET_PLATFORM})
 message(STATUS "EngineDllName: ${EngineDllName}")
 
